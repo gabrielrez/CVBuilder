b37211aea8b6d3956881f9303b99d469aaae8bee
gabrielrez <gabrielrezcpessoa@gmail.com>
2025-03-02 11:14:43 -0300

    style: btn hover

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,11 @@
                 Create professional and personalized resumes in minutes!
             </p>
             <div class="flex gap-5">
-                <button class="mt-16 w-max bg-[#68ac99] hover:bg-[#8592bc] px-10 py-4 rounded-2xl font-[inter] font-semibold text-lg text-[#09100d] cursor-pointer transition-all">
-                    Create Resume
-                </button>
-                <button class="mt-16 w-max bg-[#E2EAEF] hover:bg-[#ACC0D1] px-10 py-4 rounded-2xl font-[inter] font-normal text-lg text-[#09100d] cursor-pointer transition-all">
+                <button class="mt-16 w-max bg-[#68ac99] px-10 py-4 rounded-2xl font-[inter] font-semibold text-lg text-[#09100d] cursor-pointer transition-all 
+                bg-[linear-gradient(to_right,#68ac99,#68ac99)] hover:bg-[linear-gradient(to_right,#68ac99,#8592bc)]">Create Resume</button>            
+                <a href="https://github.com/gabrielrez/CVMaker" target="_blank" class="mt-16 w-max bg-[#E2EAEF] hover:bg-[#ACC0D1] px-10 py-4 rounded-2xl font-[inter] font-normal text-lg text-[#09100d] cursor-pointer transition-all">
                     <i class="fa-brands fa-github mr-2"></i> Repository
-                </button>
+                </a>
             </div>
         </div>
         <img src="{{asset('img/resume.svg')}}" alt="resume example" class="w-full">
